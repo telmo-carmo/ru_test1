02ddc3732c16e6c795fa5635390aa0c1dad3e487
Telmo Carmo <telmo.carmo@gmail.com>
2025-02-06 11:15:26 +0000

    done cmt:x

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::time::SystemTime;
 fn main() {
     std::env::set_var("RUST_LOG", "ru_test1"); // log ALL
     env_logger::init();
-    println!("Hello,{}", "TEST" );
+    println!("Hello,{}", "TEST");
     let now = SystemTime::now();
     log::warn!("This is now {now:?}");
     log::debug!("THE END!");
